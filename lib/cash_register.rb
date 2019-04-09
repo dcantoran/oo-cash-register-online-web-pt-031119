68c0cdb6391ae2e50f76c1918550b2e245155dd5
David Cantoran <davidcantoran36@gmail.com>
2019-04-09 00:55:50 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -9,7 +9,6 @@ class CashRegister
   
   def add_item(title, price, quantity=1)
     self.total += price * quantity
-    @last_trans = @total
     while quantity > 0 
       @items << title 
       quantity -= 1
